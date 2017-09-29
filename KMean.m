7b742acc0deaad1c71db662fd29266456387684a
julielaurent <32266931+julielaurent@users.noreply.github.com>
2017-09-29 23:58:13 +0200

    KMean

--- KMean.m
+++ KMean.m
@@ -2,7 +2,7 @@ function [ u ] = KMeans( centersInit, K, N, sampleVec )
 %UNTITLED2 Summary of this function goes here
 %   u -> center of cluster
 %   s -> vector of samples
-%   n -> iterationon samples
+%   n -> iteration on samples
 %   N -> nombre total de samples
 %   k -> iteration on clusters
 %   K -> nombre tot de clusers
@@ -11,7 +11,7 @@ function [ u ] = KMeans( centersInit, K, N, sampleVec )
 u = centersInit; %vector with initialization of all centers
 u0 = zeros(size(u));
 r = zeros(N,K);
-dist = zeros(K);
+dist = zeros(N,K);
 s = sampleVec;
 
 while (abs(norm((u-u0))) < 10^-8)
@@ -22,15 +22,16 @@ while (abs(norm((u-u0))) < 10^-8)
 
         for k = 1:K
         
-            dist(k,n) = (norm(s(n) - u(k)))^2;
+            dist(n,k) = (norm(s(n) - u(k)))^2;
         
         end
     
-        r(n,find(min(dist(:,n)))) = 1;
+        r(n,find(min(dist(n,:)))) = 1;
     
     end
     
-    u(k) = sum(r(n,k)*s(n))./sum(r(n,k));
+    u = sum(r.*s) ./ sum(r);
+    %u(k) = sum(r(n,k)*s(n))./sum(r(n,k));
 
 end
     
